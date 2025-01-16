d79a34d50d1372224215fe47e787a918098c0ea4
Asril <ardiaslintang03@gmail.com>
2025-01-16 18:59:09 +0700

    Normalize line endings

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color formColor = const Color(0XFFFAFAFA);
+Color blueColor = const Color(0xFF4285F4);
+Color greyColor = const Color(0xFF736A6A);
+Color lightgreyColor = const Color(0xFFF3EDF7);
+Color darkgreyColor = const Color(0xFFE8DEF8);
